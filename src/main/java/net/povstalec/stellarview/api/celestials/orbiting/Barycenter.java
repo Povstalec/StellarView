fb5681b6fda969fdc956f4e62fc2daeec570f783
Noobly-Walker <pikatheminecraftgamer@gmail.com>
2024-05-28 07:48:41 -0500

    Split Barycenter fix into separate pull

--- src/main/java/net/povstalec/stellarview/api/celestials/orbiting/Barycenter.java
+++ src/main/java/net/povstalec/stellarview/api/celestials/orbiting/Barycenter.java
@@ -8,10 +8,18 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
 
-public class Barycenter extends OrbitingCelestialObject
+public final class Barycenter extends OrbitingCelestialObject
 {
 	public Barycenter()
 	{
-		super(Planet.VENUS_TEXTURE, 0);
+		super(null, 0);
+	}
+
+	// Barycenter is invisible, no point in rendering it
+	@Override
+	public void render(OrbitingCelestialObject viewCenter, Vector3f vievCenterCoords, ClientLevel level, Camera camera, float partialTicks, PoseStack stack, BufferBuilder bufferbuilder,
+					   Vector3f skyAxisRotation, Vector3f parentCoords)
+	{
+
 	}
 }
