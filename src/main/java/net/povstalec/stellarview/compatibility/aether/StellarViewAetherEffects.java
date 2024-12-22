6f393be9bbc8cdea026fff4af57fcca129148fd7
Povstalec <woldericzjunior@gmail.com>
2024-12-22 17:48:01 +0100

    Aether compatibility

--- src/main/java/net/povstalec/stellarview/compatibility/aether/StellarViewAetherEffects.java
+++ src/main/java/net/povstalec/stellarview/compatibility/aether/StellarViewAetherEffects.java
@@ -1,5 +1,6 @@
 package net.povstalec.stellarview.compatibility.aether;
 
+import com.aetherteam.aether.client.renderer.level.AetherSkyRenderEffects;
 import net.minecraft.client.Camera;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.DimensionSpecialEffects;
@@ -8,12 +9,13 @@ import net.minecraft.world.phys.Vec3;
 import net.povstalec.stellarview.StellarView;
 import net.povstalec.stellarview.client.render.level.util.StellarViewLightmapEffects;
 import net.povstalec.stellarview.client.resourcepack.ViewCenters;
+import net.povstalec.stellarview.common.config.AetherConfig;
 import net.povstalec.stellarview.common.config.TwilightForestConfig;
 import net.povstalec.stellarview.compatibility.enhancedcelestials.EnhancedCelestialsCompatibility;
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
 
-public class StellarViewAetherEffects extends DimensionSpecialEffects.OverworldEffects
+public class StellarViewAetherEffects extends AetherSkyRenderEffects
 {
 	public static final ResourceLocation AETHER_EFFECTS = ResourceLocation.fromNamespaceAndPath(StellarView.AETHER_MODID, "the_aether");
 	
@@ -22,7 +24,7 @@ public class StellarViewAetherEffects extends DimensionSpecialEffects.OverworldE
 	@Override
 	public boolean renderSky(ClientLevel level, int ticks, float partialTick, Matrix4f modelViewMatrix, Camera camera, Matrix4f projectionMatrix, boolean isFoggy, Runnable setupFog)
     {
-		if(TwilightForestConfig.replace_default.get())
+		if(AetherConfig.replace_default.get())
 			return ViewCenters.renderViewCenterSky(level, ticks, partialTick, modelViewMatrix, camera, projectionMatrix, isFoggy, setupFog);
 		
         return super.renderSky(level, ticks, partialTick, modelViewMatrix, camera, projectionMatrix, isFoggy, setupFog);
@@ -31,7 +33,7 @@ public class StellarViewAetherEffects extends DimensionSpecialEffects.OverworldE
 	@Override
 	public void adjustLightmapColors(ClientLevel level, float partialTicks, float skyDarken, float skyLight, float blockLight, int pixelX, int pixelY, Vector3f colors)
     {
-		if(TwilightForestConfig.replace_default.get())
+		if(AetherConfig.replace_default.get())
 		{
 			StellarViewLightmapEffects.defaultLightmapColors(level, partialTicks, skyDarken, skyLight, blockLight, pixelX, pixelY, colors);
 			
