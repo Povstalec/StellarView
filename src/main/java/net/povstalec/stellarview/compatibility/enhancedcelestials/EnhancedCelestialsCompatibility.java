4f49cab72d2b8f266e0909944c4a74f49e2e326e
Povstalec <woldericzjunior@gmail.com>
2025-05-16 17:54:31 +0200

    Enhanced Celestials Compatibility

--- src/main/java/net/povstalec/stellarview/compatibility/enhancedcelestials/EnhancedCelestialsCompatibility.java
+++ src/main/java/net/povstalec/stellarview/compatibility/enhancedcelestials/EnhancedCelestialsCompatibility.java
@@ -1,5 +1,10 @@
 package net.povstalec.stellarview.compatibility.enhancedcelestials;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import dev.corgitaco.enhancedcelestials.EnhancedCelestials;
+import dev.corgitaco.enhancedcelestials.api.client.ColorSettings;
+import dev.corgitaco.enhancedcelestials.api.lunarevent.LunarEvent;
+import dev.corgitaco.enhancedcelestials.client.ECWorldRenderer;
 import org.joml.Vector3f;
 import org.joml.Vector3fc;
 
@@ -10,45 +15,40 @@ import net.minecraft.util.Mth;
 import net.povstalec.stellarview.client.render.level.util.StellarViewLightmapEffects;
 import net.povstalec.stellarview.common.util.Color;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 public class EnhancedCelestialsCompatibility
 {
 	public static final float getMoonSize(ClientLevel level, float defaultSize)
 	{
-		return 20;//ECWorldRenderer.getMoonSize(defaultSize);
+		return ECWorldRenderer.getMoonSize(defaultSize);
 	}
 	
 	public static final Color.FloatRGBA getMoonColor(ClientLevel level, float partialTicks)
 	{
+		AtomicReference<Color.FloatRGBA> result = new AtomicReference<>(new Color.FloatRGBA(1, 1, 1));
 		/*
 		 * Shamelessly copy pasted from
-		 * https://github.com/CorgiTaco/Enhanced-Celestials/blob/1.20.X/Common/src/main/java/corgitaco/enhancedcelestials/client/ECWorldRenderer.java#L19C9-L34C91
+		 * https://github.com/CorgiTaco/Enhanced-Celestials/blob/1.20.X/common/src/main/java/dev/corgitaco/enhancedcelestials/client/ECWorldRenderer.java
 		 * because what else am I supposed to do to have it work the same way for the sake of compatibility?
 		 */
-		/*EnhancedCelestialsWorldData enhancedCelestialsWorldData = (EnhancedCelestialsWorldData) level;
-        if(enhancedCelestialsWorldData != null)
-        {
-    		EnhancedCelestialsContext enhancedCelestialsContext = enhancedCelestialsWorldData.getLunarContext();
-        	if(enhancedCelestialsContext != null)
-            {
-                LunarForecast lunarForecast = enhancedCelestialsContext.getLunarForecast();
-                
-                ColorSettings lastColorSettings = lunarForecast.lastLunarEvent().value().getClientSettings().colorSettings();
-                ColorSettings currentColorSettings = lunarForecast.currentLunarEvent().value().getClientSettings().colorSettings();
-                
-                Vector3f lastGLColor = lastColorSettings.getGLMoonColor();
-                Vector3f currentGLColor = currentColorSettings.getGLMoonColor();
-                
-                float blend = lunarForecast.getBlend();
-                
-                float r = Mth.clampedLerp(lastGLColor.x(), currentGLColor.x(), blend);
-                float g = Mth.clampedLerp(lastGLColor.y(), currentGLColor.y(), blend);
-                float b = Mth.clampedLerp(lastGLColor.z(), currentGLColor.z(), blend);
-                
-                return new Color.FloatRGBA(r > 1F ? 1F : r, g > 1F ? 1F : g, b > 1F ? 1F : b);
-            }
-        }*/
-        
-        return new Color.FloatRGBA(1, 1, 1);
+		EnhancedCelestials.lunarForecastWorldData(level).ifPresent(data -> {
+			ColorSettings lastColorSettings = data.lastLunarEventHolder().value().getClientSettings().colorSettings();
+			ColorSettings currentColorSettings = data.currentLunarEventHolder().value().getClientSettings().colorSettings();
+			
+			Vector3f lastGLColor = lastColorSettings.getGLMoonColor();
+			Vector3f currentGLColor = currentColorSettings.getGLMoonColor();
+			
+			float blend = data.getBlend();
+			
+			float r = Mth.clampedLerp(lastGLColor.x(), currentGLColor.x(), blend);
+			float g = Mth.clampedLerp(lastGLColor.y(), currentGLColor.y(), blend);
+			float b = Mth.clampedLerp(lastGLColor.z(), currentGLColor.z(), blend);
+			RenderSystem.setShaderColor(r, g, b, 1.0F - level.getRainLevel(partialTicks));
+			
+			result.set(new Color.FloatRGBA(r > 1F ? 1F : r, g > 1F ? 1F : g, b > 1F ? 1F : b));
+		});
+		return result.get();
 	}
 	
 	public static final void adjustLightmapColors(ClientLevel level, float partialTicks, float skyDarken, float skyLight, float blockLight, int pixelX, int pixelY, Vector3f colors)
@@ -68,34 +68,25 @@ public class EnhancedCelestialsCompatibility
 			
 			/*
 			 * Shamelessly copy pasted from
-			 * https://github.com/CorgiTaco/Enhanced-Celestials/blob/1.20.X/Common/src/main/java/corgitaco/enhancedcelestials/client/ECWorldRenderer.java#L61
+			 * https://github.com/CorgiTaco/Enhanced-Celestials/blob/1.20.X/common/src/main/java/dev/corgitaco/enhancedcelestials/client/ECWorldRenderer.java
 			 * because, again, what else am I supposed to do to have it work the same way?
 			 */
-			/*EnhancedCelestialsWorldData enhancedCelestialsWorldData = (EnhancedCelestialsWorldData) level;
-			if(enhancedCelestialsWorldData != null)
-			{
-				EnhancedCelestialsContext enhancedCelestialsContext = enhancedCelestialsWorldData.getLunarContext();
-				if(enhancedCelestialsContext != null)
-				{
-					LunarForecast lunarForecast = enhancedCelestialsContext.getLunarForecast();
-					LunarEvent lastEvent = lunarForecast.lastLunarEvent().value();
-					LunarEvent currentEvent = lunarForecast.currentLunarEvent().value();
-
-					ColorSettings colorSettings = currentEvent.getClientSettings().colorSettings();
-					ColorSettings lastColorSettings = lastEvent.getClientSettings().colorSettings();
-
-					Vector3f glSkyLightColor = lastColorSettings.getGLSkyLightColor();
-					Vector3f targetColor = new Vector3f(glSkyLightColor.x(), glSkyLightColor.y(), glSkyLightColor.z());
-
-					skyDarken = (level.getSkyDarken(1.0F) - 0.2F) / 0.8F;
-					float eventBlend = lunarForecast.getBlend() - skyDarken;
-					targetColor.lerp(colorSettings.getGLSkyLightColor(), eventBlend);
-
-					float skyBlend = (1 - skyDarken) - level.getRainLevel(partialTicks);
-		            
-					skyVector.lerp(targetColor, skyBlend);
-				}
-			}*/
+			EnhancedCelestials.lunarForecastWorldData(level).ifPresent(data -> {
+				LunarEvent lastEvent = data.lastLunarEvent();
+				LunarEvent currentEvent = data.currentLunarEvent();
+				
+				ColorSettings colorSettings = currentEvent.getClientSettings().colorSettings();
+				ColorSettings lastColorSettings = lastEvent.getClientSettings().colorSettings();
+				
+				Vector3f glSkyLightColor = lastColorSettings.getGLSkyLightColor();
+				Vector3f targetColor = new Vector3f(glSkyLightColor.x(), glSkyLightColor.y(), glSkyLightColor.z());
+				
+				float eventBlend = data.getBlend() - skyDarken;
+				targetColor.lerp(colorSettings.getGLSkyLightColor(), eventBlend);
+				
+				float skyBlend = (1 - skyDarken) - level.getRainLevel(partialTicks);
+				skyVector.lerp(targetColor, skyBlend);
+			});
 			
 			Vector3f lightColor = new Vector3f();
 			float naturalLight = LightTexture.getBrightness(level.dimensionType(), pixelY) * trueSkyDarken; // pixelY represents natural light
