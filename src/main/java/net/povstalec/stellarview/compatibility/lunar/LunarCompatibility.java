a62ff5511fd0038c9e9fb774edd83b2f2b658e21
Povstalec <woldericzjunior@gmail.com>
2025-09-18 14:25:32 +0200

    Adjusted Lunar compatibility

--- src/main/java/net/povstalec/stellarview/compatibility/lunar/LunarCompatibility.java
+++ src/main/java/net/povstalec/stellarview/compatibility/lunar/LunarCompatibility.java
@@ -3,28 +3,29 @@ package net.povstalec.stellarview.compatibility.lunar;
 import com.mrbysco.lunar.client.MoonHandler;
 import net.minecraft.util.FastColor;
 import net.povstalec.stellarview.common.util.Color;
-import org.joml.Matrix4f;
 
-public class LunarCompatibility {
-
-    public static float getMoonSize(float defaultSize) {
-        if(MoonHandler.isMoonScaled()){
-            Matrix4f moonScale = MoonHandler.getMoonScale();
-            try {
-                return moonScale.m00() * defaultSize;
-            } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
-                return defaultSize;
-            }
+public class LunarCompatibility
+{
+    public static float getMoonSize(float defaultSize)
+    {
+        if(MoonHandler.isMoonScaled())
+        {
+            float moonScale = MoonHandler.getRawMoonScale();
+            
+            return moonScale * defaultSize;
         }
         return defaultSize;
     }
 
-    public static Color.FloatRGBA getMoonColor() {
-        if (MoonHandler.isEventActive()) {
+    public static Color.FloatRGBA getMoonColor()
+    {
+        if(MoonHandler.isEventActive())
+        {
             int moonColor = MoonHandler.getMoonColor();
             int r = FastColor.ARGB32.red(moonColor);
             int g = FastColor.ARGB32.green(moonColor);
             int b = FastColor.ARGB32.blue(moonColor);
+            
             return new Color.FloatRGBA(r, g, b);
         }
         return new Color.FloatRGBA(255, 255, 255);
