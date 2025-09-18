ffa06287fa22982391c614b6ff148503bbf2c49b
Povstalec <woldericzjunior@gmail.com>
2025-09-18 14:22:51 +0200

    Adjusted Lunar compatibility

--- src/main/java/net/povstalec/stellarview/compatibility/lunar/LunarCompatibility.java
+++ src/main/java/net/povstalec/stellarview/compatibility/lunar/LunarCompatibility.java
@@ -3,46 +3,30 @@ package net.povstalec.stellarview.compatibility.lunar;
 import com.mrbysco.lunar.client.MoonHandler;
 import net.minecraft.util.FastColor;
 import net.povstalec.stellarview.common.util.Color;
-import org.joml.Matrix4f;
-import java.lang.reflect.Field;
 
 public class LunarCompatibility {
 
-    public static float getMoonSize(float defaultSize) {
-        /* if(MoonHandler.isMoonScaled()){
-            Matrix4f moonScale = MoonHandler.getMoonScale();
-            try {
-                return moonScale.m00() * defaultSize;
-            } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
-                return defaultSize;
-            }
-        } */
-        // BODGE FIX - LUNAR SCALE NOT IMPLEMENTED
-        try {
-            Field moonEventID = MoonHandler.class.getDeclaredField("moonID");
-            moonEventID.setAccessible(true);
-            String eventID = (String) moonEventID.get(moonEventID);
-            float moonScale = 1;
-            if(eventID.equals("lunar:big_moon")) { moonScale = 4f; }
-            if(eventID.equals("lunar:tiny_moon")) { moonScale = 0.25f; }
+    public static float getMoonSize(float defaultSize)
+    {
+        if(MoonHandler.isMoonScaled())
+        {
+            float moonScale = MoonHandler.getRawMoonScale();
+            
             return moonScale * defaultSize;
-        } catch (NoSuchFieldException | IllegalAccessException | NullPointerException e) {
-            return defaultSize;
         }
+        return defaultSize;
     }
 
-    public static Color.FloatRGBA getMoonColor() {
-        /* Once Lunar updates to 0.2.3, there should be a getter for this,
-        so I shouldn't need to steal a private float array. -NW */
-        if (MoonHandler.isEventActive()) {
-            try {
-                Field moonColorField = MoonHandler.class.getDeclaredField("moonColor");
-                moonColorField.setAccessible(true);
-                float[] rawColor = (float[]) moonColorField.get(moonColorField);
-                return new Color.FloatRGBA(rawColor[0], rawColor[1], rawColor[2]);
-            } catch (NoSuchFieldException | IllegalAccessException | NullPointerException e) {
-                return new Color.FloatRGBA(255, 255, 255);
-            }
+    public static Color.FloatRGBA getMoonColor()
+    {
+        if(MoonHandler.isEventActive())
+        {
+            int moonColor = MoonHandler.getMoonColor();
+            int r = FastColor.ARGB32.red(moonColor);
+            int g = FastColor.ARGB32.green(moonColor);
+            int b = FastColor.ARGB32.blue(moonColor);
+            
+            return new Color.FloatRGBA(r, g, b);
         }
         return new Color.FloatRGBA(255, 255, 255);
     }
