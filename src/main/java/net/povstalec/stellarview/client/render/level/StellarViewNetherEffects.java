f6835d7f8ae42d6cbe0289c4265499f1f8d9282d
Povstalec <woldericzjunior@gmail.com>
2025-11-02 12:50:56 +0100

    Update StellarViewNetherEffects.java

--- src/main/java/net/povstalec/stellarview/client/render/level/StellarViewNetherEffects.java
+++ src/main/java/net/povstalec/stellarview/client/render/level/StellarViewNetherEffects.java
@@ -8,6 +8,7 @@ import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.DimensionSpecialEffects;
 import net.minecraft.resources.ResourceLocation;
 import net.povstalec.stellarview.api.client.render.level.StellarViewSpecialEffects;
+import net.povstalec.stellarview.client.render.ViewCenters;
 import net.povstalec.stellarview.common.config.NetherConfig;
 
 import javax.annotation.Nullable;
